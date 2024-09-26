6c84e24e3089becc86f6defa0f92c4ac7f38c2c5
Omkar Mandavkar <73053306+OmkarMandavkar@users.noreply.github.com>
2024-09-26 11:18:47 +0530

    Update closeConnectionEg2.java

--- src/main/java/com/option/closeConnection/closeConnectionEg2.java
+++ src/main/java/com/option/closeConnection/closeConnectionEg2.java
@@ -7,36 +7,28 @@ import java.sql.Statement;
 
 public class closeConnectionEg2 {
 	public static void main(String[] args) {
-		String url = "jdbc:postgresql://localhost:5432/course";
-		String user = "postgres";
-		String password = "root";
+		String url = "jdbc:postgresql://localhost:5432/course?user=postgres&password=root";
 		String driver = "org.postgresql.Driver";
-		Connection connection = null;
 
 		try {
-			Class.forName(driver)
+			Class.forName(driver);
 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		} 
-		
-		
-		try() {
-			
 		}
+
+		try (Connection connection = DriverManager.getConnection(url)) {
+
+			Statement smt = connection.createStatement();
+
+			String sql = "SELECT * FROM student";
+			smt.execute(sql);
+
+			System.out.println("Query Executed");
+
+		} 
 		catch (SQLException e) {
 			e.printStackTrace();
-		} 
-		finally {
-			try {
-				if (connection != null) {
-					connection.close();
-					System.out.println("Query Executed and Connection Closed");
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
 		}
 	}
 }
